8431f1d510971df6e66a124f3b5fd318fe78ba6b
Vulcan <vulcan.lee@gmail.com>
2024-06-07 13:33:54 +0800

    加入核心服務的支援類別

--- src/TutorialBlazor/TutorialBlazor/Program.cs
+++ src/TutorialBlazor/TutorialBlazor/Program.cs
@@ -1,36 +1,48 @@
 using TutorialBlazor.Components;
+using TutorialBlazor.Startup;
 
-namespace TutorialBlazor
+namespace TutorialBlazor;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddCoreServices();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
     }
 }
---
+++ src/TutorialBlazor/TutorialBlazor/Startup/CoreService.cs
@@ -0,0 +1,25 @@
+﻿namespace TutorialBlazor.Startup;
+
+public static class CoreService
+{
+    /// <summary>
+    /// 加入核心服務
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    {
+        #region 加入支援 Web API 的註冊 & 支援 Swagger
+        services.AddConnections();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        #endregion
+
+        #region 加入支援 Razor 元件與互動式的 Server SSR
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+        #endregion
+       
+        return services;
+    }
+}
